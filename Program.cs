94f1af4717b16c5e58b1cccf16459e073a8b3eee
Name <vbrfqlj@mail.ru>
2025-02-15 14:48:58 +0300

    Задание 5.3.13. Создание методов SortArrayDesc и SortArrayAsc

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
     static void Main(string[] args)
     {
         var num = 6;
+        var sorteddesc = new int[0];
+        var sortedasc = new int[0];
 
         var array = GetArrayFromConsole(ref num);
 
-        var sortarray = SortArray(array);
+        SortArray(array,out sorteddesc,out sortedasc);
     }
     static int[] GetArrayFromConsole(ref int num)
     {
@@ -20,15 +24,24 @@ class Program
             result[i] = int.Parse(Console.ReadLine());
         }
 
+        Console.WriteLine("Ваш массив:");
+
         for (int i = 0; i < result.Length; i++)
         {
             Console.WriteLine(result[i]);
         }
-
         return result;
     }
 
-    static int[] SortArray(int[] array)
+    static void SortArray(int[] array, out int[] sorteddesc, out int[] sortedasc)
+    {
+        sorteddesc = SortArrayDesc(array);
+
+        sortedasc = SortArrayAsc(array);
+
+    }
+
+    static int[] SortArrayAsc(int[] array)
     {
         int k = 0;
         for (int i = 0; i < array.Length; i++)
@@ -44,7 +57,35 @@ class Program
             }
 
         }
-        Console.WriteLine("Массив после сортировки:");
+
+        Console.WriteLine("Массив после сортировки по возрастанию:");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+
+        return array;
+    }
+
+    static int[] SortArrayDesc(int[] array)
+    {
+        int k = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    k = array[i];
+                    array[i] = array[j];
+                    array[j] = k;
+                }
+            }
+
+        }
+
+        Console.WriteLine("Массив после сортировки по убыванию:");
 
         for (int i = 0; i < array.Length; i++)
         {
